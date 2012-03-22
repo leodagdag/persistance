1721a98a64771139b9588fdb12c2f4be648e8aff
Frédéric Patin <leodagdag@gmail.com>
2012-03-22 17:05:28 +0100

    Add play mongodb jackson module

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,13 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+      "net.vz.mongodb.jackson" % "play-mongo-jackson-mapper_2.9.1" % "1.0.0-rc.2",
       "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
       // "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA)
+    .settings(
       lessEntryPoints <<= baseDirectory(customLessEntryPoints)
     )
 
